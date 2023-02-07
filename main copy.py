d21fff692883ed8669d3671a5cc239f965a556f3
Kuba Pezda <kuba.pezda@interia.pl>
2023-02-07 12:05:54 +0100

    asdfg

--- main copy.py
+++ main copy.py
@@ -7,6 +7,7 @@ import multiprocessing as mp
 from multiprocessing.shared_memory import SharedMemory
 from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
 import time
+from copy import deepcopy
 
 
 
@@ -38,15 +39,15 @@ if __name__ == '__main__':
 	# layer_file = 'CS-Aarhus_layers.txt'
 	# nodes_name = 'CS-Aarhus_nodes.txt'
 	
-	dir = join('dane','EUAir_Multiplex_Transport','Dataset')
-	edge_file = 'EUAirTransportation_multiplex.edges'
-	layer_file = 'EUAirTransportation_layers.txt'
-	nodes_name = 'EUAirTransportation_nodes.txt'
+	# dir = join('dane','EUAir_Multiplex_Transport','Dataset')
+	# edge_file = 'EUAirTransportation_multiplex.edges'
+	# layer_file = 'EUAirTransportation_layers.txt'
+	# nodes_name = 'EUAirTransportation_nodes.txt'
 
-	# dir = join('dane','HumanMicrobiome_Multiplex_Biological','Dataset')
-	# edge_file = 'HumanMicrobiome_multiplex.edges'
-	# layer_file = 'HumanMicrobiome_layers.txt'
-	# nodes_name = 'HumanMicrobiome_nodes.txt'
+	dir = join('dane','HumanMicrobiome_Multiplex_Biological','Dataset')
+	edge_file = 'HumanMicrobiome_multiplex.edges'
+	layer_file = 'HumanMicrobiome_layers.txt'
+	nodes_name = 'HumanMicrobiome_nodes.txt'
 
 
 
@@ -74,21 +75,15 @@ if __name__ == '__main__':
 	stops = [i*(M//N) for i in range(N)]
 	stops[-1] = M
 
-	
+
 
 
 	# dla każdej pary poziomów w sieci oblicz pairwise multiplexity i ustaw jako wagę krawędzi
 	G = nx.Graph()
 	with ProcessPoolExecutor(N) as pool:
 		for i,j in itertools.combinations(range(len(layers)), 2):
-			results = pool.map(pair_multiplexity,
-				itertools.repeat(arr.shape,len(stops)-1),
-				itertools.repeat(i,len(stops)-1),
-				itertools.repeat(j,len(stops)-1),
-				stops[:-1],
-				stops[1:])
-
-			weight = sum([result for result in results]) / M
+			futures = [pool.submit(pair_multiplexity, arr.shape, i, j, stops[k], stops[k+1]) for k in range(len(stops) - 1)]
+			weight = sum([future.result() for future in futures])/M
 
 			G.add_edge(i,j, weight = weight)
 
@@ -98,7 +93,7 @@ if __name__ == '__main__':
  
 	# przeskalowanie wag na grubości krawędzi
 	weights = list( nx.get_edge_attributes(G, 'weight').values() )
-	scaled = [w*10 for w in weights]
+	scaled = [w*w*100 for w in weights]
 	
 	pos = nx.circular_layout(G)
 	# pos = nx.spring_layout(G)
@@ -106,7 +101,9 @@ if __name__ == '__main__':
 
 	nx.draw_networkx_nodes(G, pos)
 	nx.draw_networkx_labels(G,pos,layers, bbox=dict(facecolor='yellow', alpha=.25))#, font_weight='bold'
-	nx.draw_networkx_edges(G,pos, width=scaled, edge_color='#707070')	#, connectionstyle='Angle, angleA=90, angleB=20, rad=0.2'  , edge_color='#4169e1'
+	nx.draw_networkx_edges(G,pos, width=scaled, edge_color='#6189e1')	#, connectionstyle='Angle, angleA=90, angleB=20, rad=0.2'  , edge_color='#4169e1' '#707070'
  
 	finish = time.time()
 	print(f'Czas wykonania: {finish-start}')
+
+	# plt.show()
--- main.py
+++ main.py
@@ -17,15 +17,15 @@ def main():
 
 	# wybór danych
 
-	# dir = join('dane','CS-Aarhus_Multiplex_Social','Dataset')
-	# edge_file = 'CS-Aarhus_multiplex.edges'
-	# layer_file = 'CS-Aarhus_layers.txt'
-	# nodes_name = 'CS-Aarhus_nodes.txt'
+	dir = join('dane','CS-Aarhus_Multiplex_Social','Dataset')
+	edge_file = 'CS-Aarhus_multiplex.edges'
+	layer_file = 'CS-Aarhus_layers.txt'
+	nodes_name = 'CS-Aarhus_nodes.txt'
 	
-	dir = join('dane','EUAir_Multiplex_Transport','Dataset')
-	edge_file = 'EUAirTransportation_multiplex.edges'
-	layer_file = 'EUAirTransportation_layers.txt'
-	nodes_name = 'EUAirTransportation_nodes.txt'
+	# dir = join('dane','EUAir_Multiplex_Transport','Dataset')
+	# edge_file = 'EUAirTransportation_multiplex.edges'
+	# layer_file = 'EUAirTransportation_layers.txt'
+	# nodes_name = 'EUAirTransportation_nodes.txt'
 
 	# dir = join('dane','HumanMicrobiome_Multiplex_Biological','Dataset')
 	# edge_file = 'HumanMicrobiome_multiplex.edges'
@@ -42,28 +42,16 @@ def main():
 	# pozyskanie wielopoziomowej macierzy sąsiedztwa
 	arr = edges_to_array(edges, len(layers), len(nodes))
 
-	# Przygotowanie egzekutora
-	N = mp.cpu_count()
-	pool = ThreadPoolExecutor(N)
-	# pool = ProcessPoolExecutor(N)
 
-	# przygotowanie zakresów dla wątków/procesów
-	M = len(nodes)
-	stops = [i*(M//N) for i in range(N)]
-	stops[-1] = M
 
 	# dla każdej pary poziomów w sieci oblicz pairwise multiplexity i ustaw jako wagę krawędzi
 	G = nx.Graph()
-	# with ProcessPoolExecutor(N) as pool:
 	for i,j in itertools.combinations(range(len(layers)), 2):
-		futures = [pool.submit(pair_multiplexity, arr, i, j, stops[k], stops[k+1])			for k in range(len(stops) - 1)]
-		weight = sum([future.result() for future in futures]) / M
-
-		G.add_edge(i,j, weight = weight)
+		G.add_edge(i,j, weight = pair_multiplexity(arr, i, j, len(nodes)))
 
 	# przeskalowanie wag na grubości krawędzi
 	weights = list( nx.get_edge_attributes(G, 'weight').values() )
-	scaled = [w*10 for w in weights]
+	scaled = [w*w*10 for w in weights]
 	
 	pos = nx.circular_layout(G)
 	# pos = nx.spring_layout(G)
@@ -85,14 +73,14 @@ def main():
 
 
 
-def pair_multiplexity(matrix, a_layer, b_layer, start, stop):
+def pair_multiplexity(matrix, a_layer, b_layer, nodes):
 	out = 0
-	for i in range(start,stop):
+	for i in range(nodes):
 		a = 1 if 1 in matrix[a_layer,i] else 0
 		b = 1 if 1 in matrix[b_layer,i] else 0
 		out += (a*b)
 
-	return out
+	return out/nodes
 
 
 
